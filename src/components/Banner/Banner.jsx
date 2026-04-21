e0dd7272f339ce889109b03770b101218e68e644
farman-arefin-outlook <farman.arefin1@outlook.com>
2026-04-21 22:23:56 +0600

    Changes to gadgets

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -9,7 +9,7 @@ const Banner = () => {
             <section className='space-y-5'>
                 <div className="badge badge-soft badge-primary"><FaRegCircleDot /> New AI Powered Tools available</div>
                 <h1 className='font-bold text-6xl'>Supercharge Your<br></br>Analog Workflow</h1>
-                <p className='text-[#627382] text-wrap'>Access premium AI tools, design assets, templates, and productivity software—all in one place. Start creating faster today.Explore Products</p>
+                <p className='text-[#627382] text-wrap'>Access premium Sport Gadgets, design assets, templates, and productivity software—all in one place. Start creating faster today.Explore Products</p>
 
                <div className='flex flex-col md:flex-row gap-2'>
                 <button className='btn bg-linear-to-r from-[#4F39F6] to-[#9514FA] rounded-full text-white'>Explore Products<FaArrowRight /></button>
